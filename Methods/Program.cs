8816eeb599a0fc1396554dc5b365f7bb4c4a3ff6
Anette Kniberg <anettekniberg@gmail.com>
2020-10-27 15:59:57 +0100

    A working version of the exercise.

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -1,12 +1,32 @@
-﻿using System;
+﻿/*
+ * First "quick n dirty " version of exercise 1. 
+ * A program that reads 2 integers form the console and prints the largest of them using  your own method GetMax()
+ * TODO: Exception handling, atleast on user input. 
+ */
+using System;
 
 namespace Methods
-{
+{ 
     class Program
     {
+        static int GetMax(int firstNumber, int secondNumber)
+        { if (firstNumber > secondNumber)
+            {
+                return firstNumber;
+            }
+            else
+            {
+                return secondNumber;
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("First number:  ");
+            int firstNumber = int.Parse(Console.ReadLine());
+            Console.Write("Second number:  ");
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Largest number: " + GetMax(firstNumber, secondNumber));
         }
     }
 }
