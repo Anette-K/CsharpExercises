d36481b6f5173f5fcb9a8d411794f4c0dcb85e41
Anette Kniberg <anettekniberg@gmail.com>
2020-10-28 08:21:54 +0100

    Created placeholder

---
+++ Working_With_Strings2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Working_With_Strings2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Placeholder for exercise 2b");
+        }
+    }
+}
